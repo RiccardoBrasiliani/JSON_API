8259775cca7f59b1bad1434a29928a9ea37bd110
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2024-02-29 10:21:11 +0100

    Delete api directory

--- api/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get('http://localhost:8080/products')
-print(response.text)  # Stampa il testo della risposta
-print(response.json())  # Stampa il contenuto della risposta come JSON
