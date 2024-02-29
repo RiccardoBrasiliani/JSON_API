fe4998326e6035a3facc8ae7599275e3342cbc1a
RiccardoBrasiliani <94352050+RiccardoBrasiliani@users.noreply.github.com>
2024-02-29 10:21:27 +0100

    Delete APIJSON directory

--- APIJSON/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get('http://localhost:8000/products')
-print(response.text)  # Stampa il testo della risposta
-print(response)  # Stampa il contenuto della risposta come JSON
